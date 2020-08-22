81456657d03a502f89dd1eca704f71d7c6e06753
ttuom1nen <ttuominen@outlook.com>
2020-08-22 23:20:54 +0300

    Add hover effect to MenuItems

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -9,12 +9,11 @@ interface Props {
 
 const MenuItem = ({ title, imageUrl, size }: Props) => {
   return (
-    <div
-      style={{
-        backgroundImage: `url (${imageUrl})`,
-      }}
-      className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
