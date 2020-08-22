ca82e30acf89664f92f63567e8fd8ce0f63549f6
ttuom1nen <ttuominen@outlook.com>
2020-08-22 22:46:52 +0300

    Revamp HomePage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/Directory/Directory";
+import "./HomePage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
