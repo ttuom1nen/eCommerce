07a12418f097840c267fab789dcbfb3d15450e44
ttuom1nen <ttuominen@outlook.com>
2020-08-24 22:46:00 +0300

    Add Switch and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import HomePage from "./pages/HomePage/HomePage";
+import ShopPage from "./pages/ShopPage/ShopPage";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
