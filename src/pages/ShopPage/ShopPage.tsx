a37d2df1dd35626c959db05b22807145b8767b5b
ttuom1nen <ttuominen@outlook.com>
2020-08-24 22:47:32 +0300

    Add page for shop

---
+++ src/pages/ShopPage/ShopPage.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { SHOP_DATA } from "../../mockData";
+
+const ShopPage = () => {
+  const [collections] = useState(SHOP_DATA);
+  return <div></div>;
+};
+
+export default ShopPage;
