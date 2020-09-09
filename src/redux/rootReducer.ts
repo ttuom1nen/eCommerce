5d0c31dd6efb9fca7bba33860d00c98b9f465cf2
ttuom1nen <ttuominen@outlook.com>
2020-09-09 22:40:14 +0300

    Add cart reducer to root reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
+import { StoreState } from "../models";
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+export default combineReducers<StoreState>({
   user: userReducer,
+  cart: cartReducer,
 });
