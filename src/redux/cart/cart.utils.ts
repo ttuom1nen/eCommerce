62d2b195d6a0afd36b0f30fb7447c14e9b26106e
ttuom1nen <ttuominen@outlook.com>
2020-09-11 23:32:52 +0300

    Add cart utils

---
+++ src/redux/cart/cart.utils.ts
@@ -0,0 +1,20 @@
+import { ShopItem, CartItem } from "../../models";
+
+export const addItemToCart = (
+  cartItems: CartItem[],
+  itemToAdd: ShopItem
+): CartItem[] => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === itemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === itemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...itemToAdd, quantity: 1 }];
+};
