798f1fcb4e96a8b901d089a1bc9754700b534af4
ttuom1nen <ttuominen@outlook.com>
2020-09-12 13:40:06 +0300

    Show cart item count

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { ShoppingCartItem, StoreState } from "../../models";
 import "./CartIcon.styles.scss";
 
 const CartIcon = () => {
   const dispatch = useDispatch();
 
+  const cartItems: ShoppingCartItem[] = useSelector(
+    (state: StoreState) => state.cart.cartItems
+  );
+
   return (
     <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">
+        {cartItems.reduce(
+          (accumulatedQuantity: number, cartItem: ShoppingCartItem): number =>
+            accumulatedQuantity + cartItem.quantity,
+          0
+        )}
+      </span>
     </div>
   );
 };
