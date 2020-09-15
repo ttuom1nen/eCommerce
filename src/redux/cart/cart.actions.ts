108468ae101e1a7bc1cb8c3b3c934c56b0910e57
ttuom1nen <ttuominen@outlook.com>
2020-09-15 22:21:09 +0300

    Add remove item from cart action

--- src/redux/cart/cart.actions.ts
+++ src/redux/cart/cart.actions.ts
@@ -1,8 +1,9 @@
-import { ShopItem } from "../../models";
+import { ShopItem, ShoppingCartItem } from "../../models";
 
 export enum ActionTypes {
   CART_TOGGLE_HIDDEN = "CART_TOGGLE_HIDDEN",
   CART_ADD_ITEM = "CART_ADD_ITEM",
+  CART_REMOVE_ITEM = "CART_REMOVE_ITEM",
 }
 
 export const toggleCartHidden = () => ({
@@ -11,5 +12,10 @@ export const toggleCartHidden = () => ({
 
 export const addItem = (item: ShopItem) => ({
   type: ActionTypes.CART_ADD_ITEM,
+  payload: item,
+});
+
+export const removeItem = (item: ShoppingCartItem) => ({
+  type: ActionTypes.CART_REMOVE_ITEM,
   payload: item,
 });
