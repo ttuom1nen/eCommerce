fc2daeb2d63d170942caf559b9c36b14305ed463
ttuom1nen <ttuominen@outlook.com>
2020-09-19 18:08:11 +0300

    Implement state persistence using localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,28 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+import throttle from "lodash/throttle";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import rootReducer from "./redux/rootReducer";
+import { loadState, saveState } from "./localStorage.utils";
 
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistedState = loadState();
+
 const store = createStore(
   rootReducer,
+  persistedState,
   composeEnhancers(applyMiddleware(thunk))
+);
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
 );
 
 ReactDOM.render(
