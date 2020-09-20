2aa27c03a80524537c18b5e79717d088e7f16825
ttuom1nen <ttuominen@outlook.com>
2020-09-20 14:23:49 +0300

    Separate shop logic into collections overview component

---
+++ src/components/CollectionsOverview/CollectionsOverview.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import CollectionPreview from "../CollectionPreview/CollectionPreview";
+import { useSelector } from "react-redux";
+import { StoreState } from "../../models";
+
+const CollectionsOverview = () => {
+  const collections = useSelector(
+    (state: StoreState) => state.shop.collections
+  );
+
+  return (
+    <div className="collections-overview">
+      {collections.map((collection) => (
+        <CollectionPreview key={collection.id} {...collection} />
+      ))}
+    </div>
+  );
+};
+
+export default CollectionsOverview;
--- src/pages/ShopPage/ShopPage.tsx
+++ src/pages/ShopPage/ShopPage.tsx
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
-import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
-import { SHOP_DATA } from "../../mockData";
+import React from "react";
+import CollectionsOverview from "../../components/CollectionsOverview/CollectionsOverview";
 
 const ShopPage = () => {
-  const [collections] = useState(SHOP_DATA);
   return (
     <div className="shop-page">
-      {collections.map((collection) => (
-        <CollectionPreview key={collection.id} {...collection} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
