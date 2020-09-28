d67616d8f9724ff5353cb9dd9aadbaa189215900
ttuom1nen <ttuominen@outlook.com>
2020-09-28 22:04:53 +0300

    Change to styled components

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Directory from "../../components/Directory/Directory";
-import "./HomePage.styles.scss";
+import { HomePageContainer } from "./HomePage.styles";
 
 const HomePage = () => {
   return (
-    <div className="homepage">
+    <HomePageContainer>
       <Directory />
-    </div>
+    </HomePageContainer>
   );
 };
 
