7f6064e48bb344ea3d7e23c3b7e4011255d4ef59
ttuom1nen <ttuominen@outlook.com>
2020-09-28 22:53:02 +0300

    Change to styled components

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
-import "./CustomButton.styles.scss";
+import { CustomButtonContainer } from "./CustomButton.styles";
 
-interface Props {
+export interface Props {
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   children: ReactNode;
   isGoogleSignIn?: boolean;
@@ -9,21 +9,8 @@ interface Props {
   type?: "button" | "submit" | "reset";
 }
 
-const CustomButton: React.FC<Props> = ({
-  children,
-  inverted,
-  isGoogleSignIn,
-  ...otherProps
-}) => {
-  return (
-    <button
-      className={`${inverted ? "inverted" : ""}
-      ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+const CustomButton: React.FC<Props> = ({ children, ...props }) => {
+  return <CustomButtonContainer {...props}>{children}</CustomButtonContainer>;
 };
 
 export default CustomButton;
