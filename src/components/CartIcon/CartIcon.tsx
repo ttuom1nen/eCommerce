bbc0a375f063f737d068b77e10a8f4a58849a8b9
ttuom1nen <ttuominen@outlook.com>
2020-09-30 22:03:36 +0300

    Change to styled components

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { ShoppingCartItem, StoreState } from "../../models";
-import "./CartIcon.styles.scss";
+import {
+  CartContainer,
+  ShoppingIcon,
+  ItemCountContainer,
+} from "./CartIcon.styled";
 
 const CartIcon = () => {
   const dispatch = useDispatch();
@@ -13,16 +16,16 @@ const CartIcon = () => {
   );
 
   return (
-    <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">
+    <CartContainer onClick={() => dispatch(toggleCartHidden())}>
+      <ShoppingIcon />
+      <ItemCountContainer>
         {cartItems.reduce(
           (accumulatedQuantity: number, cartItem: ShoppingCartItem): number =>
             accumulatedQuantity + cartItem.quantity,
           0
         )}
-      </span>
-    </div>
+      </ItemCountContainer>
+    </CartContainer>
   );
 };
 
