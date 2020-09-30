a4c4233ba585595e33fc5429b9c789a1adf6c908
ttuom1nen <ttuominen@outlook.com>
2020-09-30 22:04:21 +0300

    Change to styled components

--- src/components/CheckoutItem/CheckoutItem.tsx
+++ src/components/CheckoutItem/CheckoutItem.tsx
@@ -7,7 +7,13 @@ import {
   removeItem,
 } from "../../redux/cart/cart.actions";
 
-import "./CheckoutItem.styles.scss";
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  TextContainer,
+  QuantityContainer,
+  RemoveButtonContainer,
+} from "./CheckoutItem.styles";
 
 interface Props {
   cartItem: ShoppingCartItem;
@@ -19,31 +25,21 @@ const CheckoutItem: React.FC<Props> = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
 
   return (
-    <div className="checkout-item">
-      <div className="image-container">
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt="item" />
-      </div>
-      <span className="name">{name}</span>
-      <span className="quantity">
-        <div
-          className="arrow"
-          onClick={() => dispatch(reduceQuantity(cartItem))}
-        >
-          &#10094;
-        </div>
-        <span className="value">{quantity}</span>
-        <div className="arrow" onClick={() => dispatch(addItem(cartItem))}>
-          &#10095;
-        </div>
-      </span>
-      <span className="price">{price}</span>
-      <div
-        className="remove-button"
-        onClick={() => dispatch(removeItem(cartItem))}
-      >
+      </ImageContainer>
+      <TextContainer>{name}</TextContainer>
+      <QuantityContainer>
+        <div onClick={() => dispatch(reduceQuantity(cartItem))}>&#10094;</div>
+        <span>{quantity}</span>
+        <div onClick={() => dispatch(addItem(cartItem))}>&#10095;</div>
+      </QuantityContainer>
+      <TextContainer>{price}</TextContainer>
+      <RemoveButtonContainer onClick={() => dispatch(removeItem(cartItem))}>
         &#10005;
-      </div>
-    </div>
+      </RemoveButtonContainer>
+    </CheckoutItemContainer>
   );
 };
 
