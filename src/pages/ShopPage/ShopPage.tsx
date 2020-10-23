4638e1e1c12f15b08fe4112aad7ff38b5d8f83fa
ttuom1nen <ttuominen@outlook.com>
2020-10-23 22:24:28 +0300

    Test out different methods of using firebase library

--- src/pages/ShopPage/ShopPage.tsx
+++ src/pages/ShopPage/ShopPage.tsx
@@ -19,10 +19,28 @@ const ShopPage = ({ match }: RouteComponentProps) => {
 
   useEffect(() => {
     const collectionRef = firestore.collection("collections");
-    collectionRef.onSnapshot(async (snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      dispatch(updateCollections(collectionsMap));
-    });
+
+    // Using fetch api (not recommended because of unnecessarily nested structure of response):
+    // const dbId = "crwn-db"
+    // fetch(`https://firestore.googleapis.com/v1/projects/${dbId}/databases/(default)/documents/collections`)
+    // .then(response => response.json())
+    // .then(collections => console.log(collections))
+
+    // Using promise pattern:
+    collectionRef.get().then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(updateCollections(collectionsMap));
+      })
+
+    // Using the observer pattern:
+    // const unsubscribe = collectionRef.onSnapshot(async (snapshot) => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   dispatch(updateCollections(collectionsMap));
+    // }, error => console.error(error));
+
+    return () => {
+      // unsubscribe();
+    };
   }, [dispatch]);
 
   return (
