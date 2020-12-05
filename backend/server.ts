6d3efc4e2840c4e0401cd202a4af966d06ef01e5
ttuom1nen <ttuominen@outlook.com>
2020-12-05 15:14:20 +0200

    Add comment

--- backend/server.ts
+++ backend/server.ts
@@ -13,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// Setting trustProtoHeader: true is for reverse proxies
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
